f05d4482c657a15147cb1dd21cd4325ff475438b
PassByYou888 <passbyyou888@outlook.com>
2022-06-22 08:32:38 +0800

    Update Z.ZDB2.Json.pas

--- Source/Z.ZDB2.Json.pas
+++ Source/Z.ZDB2.Json.pas
@@ -396,7 +396,6 @@ begin
   TmpSpace.Cipher := CoreSpace.Cipher;
   TmpSpace.Mode := smBigData;
   TmpSpace.OnNoSpace := {$IFDEF FPC}@{$ENDIF FPC}DoNoSpace;
-  TmpSpace.BuildSpace(CoreSpace.State^.Physics, BlockSize);
 
   if List.num > 0 then
     begin
