52dc074fcf19dc80cb5774b11333d0e5fab9aa76
PassByYou888 <passbyyou888@outlook.com>
2023-04-01 00:26:57 +0800

    Update Z.ZDB2.Thread.Pair_String_Stream.pas

--- Source/Z.ZDB2.Thread.Pair_String_Stream.pas
+++ Source/Z.ZDB2.Thread.Pair_String_Stream.pas
@@ -61,8 +61,12 @@ type
     procedure Async_Get_String_Fragment_C(Key_: SystemString; Source: TMem64; OnResult: TOn_Mem64_And_State_Event_C); overload;
     procedure Async_Get_String_Fragment_M(Key_: SystemString; Source: TMem64; OnResult: TOn_Mem64_And_State_Event_M); overload;
     procedure Async_Get_String_Fragment_P(Key_: SystemString; Source: TMem64; OnResult: TOn_Mem64_And_State_Event_P); overload;
-    function Get_String_Fragment(Key_: SystemString; IO_: TMS64): Boolean;                 // sync load fragment
+    function Get_String_Fragment(Key_: SystemString; IO_: TMS64): Boolean; // sync load fragment
     procedure Set_String_Fragment(Key_: SystemString; IO_: TMS64; Done_Free_IO_: Boolean); // async save fragment
+    // progress
+    function Progress: Boolean;
+    // backup
+    procedure Backup(Reserve_: Word);
     // flush
     procedure Flush;
     // fragment number
@@ -387,6 +391,16 @@ begin
   obj_.Owner_String_Fragment_Tool := self;
   obj_.String_Fragment_Pool_Ptr := String_Pool.Add(Key_, obj_, True);
   obj_.Async_Save_And_Free_Data(tmp);
+end;
+
+function TZDB2_Pair_String_Stream_Tool.Progress: Boolean;
+begin
+  Result := ZDB2_Marshal.Progress;
+end;
+
+procedure TZDB2_Pair_String_Stream_Tool.Backup(Reserve_: Word);
+begin
+  ZDB2_Marshal.Backup(Reserve_);
 end;
 
 procedure TZDB2_Pair_String_Stream_Tool.Flush;
