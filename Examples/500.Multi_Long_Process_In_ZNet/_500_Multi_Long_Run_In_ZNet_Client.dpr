299f34f265920bdadbb92f9e1a326a85962838c4
PassByYou888 <passbyyou888@outlook.com>
2023-08-09 16:04:35 +0800

    new demo

---
+++ Examples/500.Multi_Long_Process_In_ZNet/_500_Multi_Long_Run_In_ZNet_Client.dpr
@@ -0,0 +1,91 @@
+program _500_Multi_Long_Run_In_ZNet_Client;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+
+uses
+  System.SysUtils,
+  Z.Core, Z.Status, Z.PascalStrings, Z.UPascalStrings, Z.UnicodeMixedLib, Z.DFE, Z.Expression, Z.OpCode,
+  Z.Notify,
+  Z.Net, Z.Net.PhysicsIO, Z.Net.DoubleTunnelIO.NoAuth;
+
+var
+  cli_: TDT_P2PVM_NoAuth_Client;
+
+type
+  // 长处理的特点就是后台化处理+延迟化反馈,这也是最简单的多任务并行化机制
+  // 长处理机制,在c4架构中被大量使用.
+  // 长处理程序范式不容易入库,这是在项目开发中酌情使用的技术:long run算一种范式性质的技术
+
+  // 定义反馈结构
+  TLong_Run_Struct_ = record
+    user_info: U_String;
+    procedure Do_Done(d: TDFE);
+  end;
+
+  PLong_Run_Struct_ = ^TLong_Run_Struct_;
+
+procedure TLong_Run_Struct_.Do_Done(d: TDFE);
+begin
+  DoStatus('"%s" 计算完成 总共反馈 %d 条', [user_info.Text, d.Count]);
+end;
+
+procedure do_delay_send;
+var
+  p: PLong_Run_Struct_;
+  d: TDFE;
+  i: Integer;
+begin
+  new(p);
+  p^.user_info := umlPointerToStr(p);
+
+  d := TDFE.Create;
+  d.WritePointer(p); // 保存反馈指针
+  for i := 1 to 1000 * 10 do // 生成计算脚本
+      d.WriteString('%d*%d', [umlRR(1, 1000000), umlRR(1, 1000000)]);
+
+  DoStatus('"%s" 已经发送到服务器计算', [p^.user_info.Text]);
+  cli_.SendTunnel.SendDirectStreamCmd('Large_Expression', d);
+  d.Free;
+
+  if cli_.SendTunnel.CmdSendStatistics['Large_Expression'] < 10 then
+      SysPost.PostExecuteC_NP(0.1, do_delay_send);
+end;
+
+procedure cmd_Done_Large_Expression(Sender: TPeerIO; InData: TDFE);
+var
+  p: PLong_Run_Struct_;
+begin
+  p := PLong_Run_Struct_(InData.R.ReadPointer);
+  p^.Do_Done(InData);
+  Dispose(p);
+end;
+
+procedure Run_Client;
+begin
+  cli_ := TDT_P2PVM_NoAuth_Client.Create(TDTClient_NoAuth);
+  cli_.RecvTunnel.RegisterDirectStream('Done_Large_Expression').OnExecute_C := cmd_Done_Large_Expression; // 完成回调
+  cli_.QuietMode := True; // 调试用
+  cli_.Connect_P('127.0.0.1', '10999', '123456', procedure(const state: Boolean)
+    begin
+      if state then
+        begin
+          DoStatus('连接成功.');
+          do_delay_send();
+        end;
+    end);
+
+  while True do
+    begin
+      CheckThread();
+      cli_.Progress;
+      TCompute.Sleep(1);
+    end;
+end;
+
+begin
+  Run_Client();
+
+end.
---
+++ Examples/500.Multi_Long_Process_In_ZNet/_500_Multi_Long_Run_In_ZNet_Serv.dpr
@@ -0,0 +1,64 @@
+program _500_Multi_Long_Run_In_ZNet_Serv;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+
+uses
+  System.SysUtils,
+  Z.Core, Z.Status, Z.PascalStrings, Z.UPascalStrings, Z.UnicodeMixedLib, Z.DFE, Z.Expression, Z.OpCode,
+  Z.Net, Z.Net.PhysicsIO, Z.Net.DoubleTunnelIO.NoAuth;
+
+var
+  serv_: TDT_P2PVM_NoAuth_Service;
+
+  // 长处理的特点就是后台化处理+延迟化反馈,这也是最简单的多任务并行化机制
+  // 长处理机制,在c4架构中被大量使用.
+  // 长处理程序范式不容易入库,这是在项目开发中酌情使用的技术:long run算一种范式性质的技术
+procedure Do_Th_Large_Expression(ThSender: THPC_DirectStream; ThInData: TDFE);
+var
+  S_ID: Cardinal; // 反馈通道ID
+  user_data_: UInt64; // 远程结构
+  i: Integer;
+  OutData: TDFE;
+begin
+  // 这里需要记忆一下反馈通道ID,可能在计算中,远程出现断线,要摆在第一行
+  S_ID := ThSender.UserVariant;
+
+  user_data_ := ThInData.R.ReadPointer;
+  OutData := TDFE.Create;
+  OutData.WritePointer(user_data_); // 反馈指针
+  while ThInData.R.NotEnd do
+      OutData.WriteString(umlVarToStr(EvaluateExpressionValue(False, ThInData.R.ReadString)));
+
+  // 通过发送通道反馈回去,以S_ID方式反馈是安全的
+  serv_.SendTunnel.SendDirectStreamCmd(S_ID, 'Done_Large_Expression', OutData);
+  OutData.Free;
+end;
+
+procedure cmd_Large_Expression(Sender: TPeerIO; InData: TDFE);
+begin
+  // 处理转交给线程
+  RunHPC_DirectStreamC(Sender, nil, nil, serv_.DTService.GetUserDefineRecvTunnel(Sender).SendTunnelID, InData, Do_Th_Large_Expression);
+end;
+
+procedure run_serv;
+begin
+  serv_ := TDT_P2PVM_NoAuth_Service.Create(TDTService_NoAuth);
+  serv_.QuietMode := True; // 调试用
+  serv_.RecvTunnel.RegisterDirectStream('Large_Expression').OnExecute_C := cmd_Large_Expression;
+
+  serv_.StartService('0.0.0.0', '10999', '123456');
+  while True do
+    begin
+      CheckThread();
+      serv_.Progress;
+      TCompute.Sleep(1);
+    end;
+end;
+
+begin
+  run_serv;
+
+end.
